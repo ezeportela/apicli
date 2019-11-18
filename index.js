c49098c687abff067cd07f4800faae117b0cc5a5
Ezequiel Portela <ezequiel.portela@naranja.com>
2019-11-18 01:17:20 -0300

    routes command

--- index.js
+++ index.js
@@ -3,19 +3,13 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const { askGenerate } = require('./lib/inquirer');
-const {generateConfigFiles} = require('./lib/config/generate');
 const minimist = require('minimist');
 
+const {askGenerate} = require('./lib/inquirer');
+const {generateConfigFiles} = require('./lib/config');
+const {generateRoutes} = require('./lib/routes');
+
 const run = async () => {
-  clear();
-  
-  console.log(
-    chalk.yellow(
-      figlet.textSync('apicli', { horizontalLayout: 'full' })
-    )
-  );
-  
   const argv = minimist(process.argv.slice(2));
       
   if (argv._[0] === 'generate') {
@@ -24,8 +18,16 @@ const run = async () => {
     switch (result.generate) {
       case 'config':
         generateConfigFiles();
+      case 'routes':
+        generateRoutes();
     }
   }
 };
 
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('apicli', { horizontalLayout: 'full' })
+  )
+);
 run();
