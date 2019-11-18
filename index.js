6d3a878f5151e72a016a0b56e7d12706938c5638
Ezequiel Portela <ezequiel.portela@naranja.com>
2019-11-18 01:46:57 -0300

    Http Services command

--- index.js
+++ index.js
@@ -8,19 +8,32 @@ const minimist = require('minimist');
 const {askGenerate} = require('./lib/inquirer');
 const {generateConfigFiles} = require('./lib/config');
 const {generateRoutes} = require('./lib/routes');
+const {generateHttpServices} = require('./lib/httpServices');
+
 
 const run = async () => {
   const argv = minimist(process.argv.slice(2));
-      
-  if (argv._[0] === 'generate') {
-    const result = await askGenerate();
     
-    switch (result.generate) {
-      case 'config':
-        generateConfigFiles();
-      case 'routes':
-        generateRoutes();
-    }
+  switch(argv._[0]) {
+    case 'version':
+      const package = require('./package.json');
+      console.log(`Version: ${package.version}`);
+      break;
+    case 'generate':
+      const result = await askGenerate();
+      
+      switch (result.generate) {
+        case 'config':
+          generateConfigFiles();
+          break;
+        case 'routes':
+          generateRoutes();
+          break;
+        case 'http services':
+          generateHttpServices();
+          break;
+      }
+      break;
   }
 };
 
