6604959e62ae55d035587c4c5af5a282a984f5d9
Ezequiel Portela <ezequiel.portela@naranja.com>
2019-11-21 13:11:14 -0300

    Sops minor changes

--- index.js
+++ index.js
@@ -46,9 +46,10 @@ program
 program
   .command('sops')
   .alias('s')
+  .option('-c, --config <filepath>', 'path of config file', './config/.sops.yaml')
   .option('-e, --encrypt', 'encrypt file')
   .option('-d, --decrypt', 'decrypt file')
-  .option('-f, --file <string>', 'path of file', './config/prodblue.json')
+  .option('-f, --file <filepath>', 'path of file', './config/prodblue.json')
   .description('Encrypt/Decrypt files')
   .action(sops);
 
