a0794534ad7bf55fd656175a882488955889dcef
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-01-15 14:36:53 -0300

    Refactor cli

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 #!/usr/bin/env node
 const program = require('commander');
 
-const {printAppName} = require('./lib/common/bash');
 const registerVersionCommands = require('./lib/commands/version');
-const registerImportsCommands = require('./lib/commands/import');
-const registerExportsCommands = require('./lib/commands/export');
 const registerGitCommands = require('./lib/commands/git');
-const registerSopsCommands = require('./lib/commands/sops');
 const registerExecuteCommands = require('./lib/commands/execute');
-const registerInitCommands = require('./lib/commands/init');
-const registerInstallCommands = require('./lib/commands/install');
-const registerTestCommands = require('./lib/commands/test');
 const registerPublishCommands = require('./lib/commands/publish');
 
-printAppName();
-
 registerVersionCommands(program);
-
-registerImportsCommands(program);
-registerExportsCommands(program);
 registerGitCommands(program);
-registerSopsCommands(program);
 registerExecuteCommands(program);
-registerInitCommands(program);
-registerInstallCommands(program);
-registerTestCommands(program);
 registerPublishCommands(program);
 
 program.parse(process.argv);
