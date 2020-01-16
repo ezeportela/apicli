f7e076a1f1263171a54a307e3f65f281f0370868
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-01-16 13:46:57 -0300

    Install command

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const registerVersionCommands = require('./lib/commands/version');
 const registerGitCommands = require('./lib/commands/git');
 const registerExecuteCommands = require('./lib/commands/execute');
 const registerPublishCommands = require('./lib/commands/publish');
+const registerInstallCommands = require('./lib/commands/install');
 
 registerVersionCommands(program);
 registerGitCommands(program);
 registerExecuteCommands(program);
 registerPublishCommands(program);
+registerInstallCommands(program);
 
 program.parse(process.argv);
